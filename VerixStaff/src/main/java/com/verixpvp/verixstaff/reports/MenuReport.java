e1518ef099ab1605b6953a551c3fb1651b55c9f7
LockedThread <proteinshakedev@gmail.com>
2019-08-15 21:36:40 -0400

    Removed debug messages

--- VerixStaff/src/main/java/com/verixpvp/verixstaff/reports/MenuReport.java
+++ VerixStaff/src/main/java/com/verixpvp/verixstaff/reports/MenuReport.java
@@ -21,11 +21,6 @@ public class MenuReport extends Menu {
         this.reportList = reports;
         int mathSlots = getInventory().getSize() - getBlockedSlots().size();
         this.totalPages = reportList.size() / mathSlots;
-
-        System.out.println("reportList = " + reportList);
-        System.out.println("mathSlots = " + mathSlots);
-        System.out.println("getBlockedSlots() = " + getBlockedSlots());
-        System.out.println("totalPages = " + totalPages);
         initialize();
     }
 
@@ -36,24 +31,16 @@ public class MenuReport extends Menu {
     @Override
     public void initialize() {
         MenuItem previous = VerixStaff.getInstance().getUnitReport().getPreviousPageItem();
-        System.out.println("previous = " + previous.getItemStack());
         int previousSlot = VerixStaff.getInstance().getConfig().getInt("reports.menu.previous-page-item.slot");
-        System.out.println("previousSlot = " + previousSlot);
         setItem(previousSlot, previous);
         MenuItem nextPageItem = VerixStaff.getInstance().getUnitReport().getNextPageItem();
-        System.out.println("nextPageItem = " + nextPageItem.getItemStack());
         setItem(VerixStaff.getInstance().getConfig().getInt("reports.menu.next-page-item.slot"), nextPageItem);
 
-        System.out.println("getInventory().getSize() - getBlockedSlots().size() = " + (getInventory().getSize() - getBlockedSlots().size()));
         int startIndex = getStartIndex();
 
-        System.out.println("startIndex = " + startIndex);
         int endIndex = getEndIndex();
-        System.out.println("endIndex = " + endIndex);
         int slot = 0;
         for (int i = startIndex; i < endIndex; i++) {
-            System.out.println("i = " + i);
-            System.out.println("slot = " + slot);
             if (!blockedSlots.contains(slot)) {
                 setItem(slot, reportList.get(i).getIncidentItemStack());
             }
@@ -74,9 +61,7 @@ public class MenuReport extends Menu {
         }
 
         int reportListSize = reportList.size();
-        System.out.println("reportListSize = " + reportListSize);
         int size = (page + 1) * (getInventory().getSize() - getBlockedSlots().size());
-        System.out.println("size = " + size);
         if (reportListSize > size) {
             return size;
         }
