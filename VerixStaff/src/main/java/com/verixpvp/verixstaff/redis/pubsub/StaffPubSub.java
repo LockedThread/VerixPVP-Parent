c8a8e8efabbe28f42fa5a202a7cdd9808536d37c
LockedThread <proteinshakedev@gmail.com>
2019-08-24 13:29:57 -0400

    Added return to same server context

--- VerixStaff/src/main/java/com/verixpvp/verixstaff/redis/pubsub/StaffPubSub.java
+++ VerixStaff/src/main/java/com/verixpvp/verixstaff/redis/pubsub/StaffPubSub.java
@@ -16,6 +16,9 @@ public class StaffPubSub extends JedisPubSub {
     @Override
     public void onMessage(String channel, String message) {
         System.out.println("channel = [" + channel + "], message = [" + message + "]");
+        if (message.split(":")[0].equals(VerixStaff.getInstance().getServerContext())) {
+            return;
+        }
         HashMap<UUID, StaffPlayer> map = GSGCore.getInstance().getGson().fromJson(message, new TypeToken<HashMap<UUID, StaffPlayer>>() {
         }.getType());
         System.out.println("map = " + map);
@@ -23,5 +26,10 @@ public class StaffPubSub extends JedisPubSub {
         try (Jedis jedis = VerixStaff.getInstance().getJedisPool().getResource()) {
             jedis.set("online-players", GSGCore.getInstance().getGson().toJson(UnitStaffMode.getInstance().getStaffPlayerMap()));
         }
+    }
+
+    @Override
+    public void onPMessage(String pattern, String channel, String message) {
+        super.onPMessage(pattern, channel, message);
     }
 }
