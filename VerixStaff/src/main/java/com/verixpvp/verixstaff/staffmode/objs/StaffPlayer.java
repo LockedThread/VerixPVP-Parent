efe2ba635a2edbc01a3d7c95dc2605d9df2bf04e
LockedThread <proteinshakedev@gmail.com>
2019-08-24 13:31:39 -0400

    Added offlineplayer impl and made it transient for serialization

--- VerixStaff/src/main/java/com/verixpvp/verixstaff/staffmode/objs/StaffPlayer.java
+++ VerixStaff/src/main/java/com/verixpvp/verixstaff/staffmode/objs/StaffPlayer.java
@@ -2,33 +2,38 @@ package com.verixpvp.verixstaff.staffmode.objs;
 
 import com.gameservergroup.gsgcore.items.ItemStackBuilder;
 import com.gameservergroup.gsgcore.menus.MenuItem;
+import com.gameservergroup.gsgcore.utils.Text;
 import com.verixpvp.verixstaff.VerixStaff;
 import com.verixpvp.verixstaff.staffmode.units.UnitStaffMode;
+import org.bukkit.Bukkit;
 import org.bukkit.Material;
 import org.bukkit.OfflinePlayer;
 import org.bukkit.entity.Player;
 import org.bukkit.inventory.ItemStack;
 
 import java.util.Map;
-import java.util.Objects;
+import java.util.UUID;
 import java.util.stream.Collectors;
 
 public class StaffPlayer {
 
-    private final OfflinePlayer offlinePlayer;
+    private final UUID uuid;
+    private transient OfflinePlayer offlinePlayer;
     private final String serverContext;
     private boolean staffMode, staffChat;
     private Role role;
 
     public StaffPlayer(Player player) {
         this.offlinePlayer = player;
-        this.role = UnitStaffMode.getInstance().getPermissionToRoleMap()
-                .entrySet()
-                .stream()
-                .filter(entry -> player.hasPermission(entry.getKey()))
-                .findFirst()
-                .map(Map.Entry::getValue)
-                .orElse(null);
+        this.uuid = player.getUniqueId();
+        Role role = null;
+        for (Map.Entry<String, Role> entry : UnitStaffMode.getInstance().getPermissionToRoleMap().entrySet()) {
+            if (player.isPermissionSet(entry.getKey())) {
+                role = entry.getValue();
+                break;
+            }
+        }
+        this.role = role;
         this.serverContext = VerixStaff.getInstance().getServerContext();
     }
 
@@ -41,16 +46,16 @@ public class StaffPlayer {
                 .consumeItemMeta(itemMeta -> {
                     String color = "&" + role.getChatColor().getChar();
                     System.out.println("color = " + color);
-                    itemMeta.setDisplayName(itemMeta.getDisplayName().replace("{color}", color)
-                            .replace("{player}", offlinePlayer.getName())
+                    itemMeta.setDisplayName(Text.toColor(itemMeta.getDisplayName().replace("{color}", color)
+                            .replace("{player}", getOfflinePlayer().getName())
                             .replace("{server}", serverContext)
-                            .replace("{role}", role.getFormattedName()));
+                            .replace("{role}", role.getFormattedName())));
                     itemMeta.setLore(itemMeta.getLore()
                             .stream()
-                            .map(s -> s.replace("{color}", color)
-                                    .replace("{player}", offlinePlayer.getName())
+                            .map(s -> Text.toColor(s.replace("{color}", color)
+                                    .replace("{player}", getOfflinePlayer().getName())
                                     .replace("{server}", serverContext)
-                                    .replace("{role}", role.getFormattedName()))
+                                    .replace("{role}", role.getFormattedName())))
                             .collect(Collectors.toList()));
                 }).setMaterial(material)
                 .build();
@@ -59,6 +64,7 @@ public class StaffPlayer {
     }
 
     public OfflinePlayer getOfflinePlayer() {
+        if (offlinePlayer == null) offlinePlayer = Bukkit.getOfflinePlayer(uuid);
         return offlinePlayer;
     }
 
@@ -90,6 +96,14 @@ public class StaffPlayer {
         this.role = role;
     }
 
+    public UUID getUuid() {
+        return uuid;
+    }
+
+    public double getPriority() {
+        return role.getPriority();
+    }
+
     @Override
     public boolean equals(Object o) {
         if (this == o) return true;
@@ -99,18 +113,30 @@ public class StaffPlayer {
 
         if (staffMode != that.staffMode) return false;
         if (staffChat != that.staffChat) return false;
-        if (!offlinePlayer.equals(that.offlinePlayer)) return false;
+        if (!uuid.equals(that.uuid)) return false;
         if (!serverContext.equals(that.serverContext)) return false;
-        return Objects.equals(role, that.role);
+        return role.equals(that.role);
     }
 
     @Override
     public int hashCode() {
-        int result = offlinePlayer.hashCode();
+        int result = uuid.hashCode();
+        result = 31 * result + serverContext.hashCode();
         result = 31 * result + (staffMode ? 1 : 0);
         result = 31 * result + (staffChat ? 1 : 0);
-        result = 31 * result + serverContext.hashCode();
-        result = 31 * result + (role != null ? role.hashCode() : 0);
+        result = 31 * result + role.hashCode();
         return result;
+    }
+
+    @Override
+    public String toString() {
+        return "StaffPlayer{" +
+                "offlinePlayer=" + offlinePlayer +
+                ", uuid=" + uuid +
+                ", serverContext='" + serverContext + '\'' +
+                ", staffMode=" + staffMode +
+                ", staffChat=" + staffChat +
+                ", role=" + role +
+                '}';
     }
 }
